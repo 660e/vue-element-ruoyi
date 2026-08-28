04677f70a1cb99e397931fa98b3b269427a5ae4b
Min <solunar@vip.qq.com>
2026-08-28 16:06:27 +0800

    Update

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,3 +1,11 @@
+<script setup>
+// import { captchaImage } from '@/api';
+</script>
+
 <template>
-  <div></div>
+  <div class="flex h-screen items-center justify-center">
+    <el-card class="w-100">
+      <div></div>
+    </el-card>
+  </div>
 </template>
