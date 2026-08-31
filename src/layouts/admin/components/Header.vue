e79b6da6a166d984863931bd34f71096d51743a9
Min <solunar@vip.qq.com>
2026-08-31 14:32:19 +0800

    Update

--- src/layouts/admin/components/Header.vue
+++ src/layouts/admin/components/Header.vue
@@ -1,3 +1,13 @@
+<script setup>
+import headerImage from '@/assets/images/layout/header.png';
+import { globalConfig } from '@/config/global.js';
+</script>
+
 <template>
-  <header>Header</header>
+  <header :style="{ backgroundImage: `url(${headerImage})` }" class="flex h-14 shrink-0 items-center justify-between bg-cover bg-center px-3">
+    <div class="text-2xl leading-none text-white">{{ globalConfig.app.name }}</div>
+    <div>
+      <div class="h-8 w-8 rounded-full bg-white"></div>
+    </div>
+  </header>
 </template>
