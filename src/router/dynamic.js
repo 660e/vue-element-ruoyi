10748b0f638cce0bc86dec1447e0cf70e250d7f6
Min <solunar@vip.qq.com>
2026-08-31 16:59:03 +0800

    Update

--- src/router/dynamic.js
+++ src/router/dynamic.js
@@ -2,14 +2,31 @@ import { getRouters } from '@/api';
 import { useAppStore } from '@/stores';
 
 function standardizeRoutes(routes) {
-  console.log(routes);
+  return routes.map((route) => {
+    switch (route.component) {
+      case 'Layout': {
+        console.log(route);
+        break;
+      }
 
-  return [1, 2, 3];
+      case 'ParentView': {
+        // console.log(route)
+        break;
+      }
+
+      default: {
+        // console.log(route)
+        break;
+      }
+    }
+  });
 }
 
 export async function initializeRouter() {
   const { data } = await getRouters();
   const appStore = useAppStore();
 
-  appStore.setRoutes(standardizeRoutes(data));
+  console.log(standardizeRoutes(data));
+
+  appStore.setRoutes([1, 2, 3]);
 }
