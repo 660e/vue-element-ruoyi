4cc6a35a19d3d8cf5c9b7510ce200616ab7142b6
Min <solunar@vip.qq.com>
2026-09-01 10:38:06 +0800

    Update

--- src/router/dynamic.js
+++ src/router/dynamic.js
@@ -5,57 +5,43 @@ const views = import.meta.glob('@/views/**/index.vue', { eager: true });
 
 function standardizeRoutes(routes = [], parentName = '') {
   return routes.map((route) => {
-    switch (route.component) {
-      case 'Layout': {
-        if (route.meta.link) {
-          return {
-            path: '/placeholder',
-            name: 'Placeholder',
-            component: () => import('@/views/Placeholder.vue'),
-            meta: {
-              title: route.meta.title,
-              icon: route.meta.icon,
-              hidden: route.hidden,
-              frameSrc: route.meta.link,
-              frameBlank: true,
-            },
-          };
-        } else if (route.children?.length) {
-          return {
-            path: route.path,
-            name: route.name,
-            redirect: '/',
-            meta: {
-              title: route.meta.title,
-              icon: route.meta.icon,
-              hidden: route.hidden,
-            },
-            children: standardizeRoutes(route.children, route.name),
-          };
-        } else {
-          console.log(route);
-          return {};
-        }
-      }
-
-      case 'ParentView': {
-        console.log(route);
-        break;
-      }
-
-      default: {
-        const view = views[`/src/views/${route.component}.vue`];
-        return {
-          path: route.path,
-          name: `${parentName}${route.name}`,
-          component: view ? markRaw(view.default) : () => import('@/views/Placeholder.vue'),
-          meta: {
-            title: route.meta.title,
-            icon: route.meta.icon,
-            hidden: route.hidden,
-          },
-        };
-      }
+    if (route.meta.link) {
+      return {
+        path: '/placeholder',
+        name: 'Placeholder',
+        component: () => import('@/views/Placeholder.vue'),
+        meta: {
+          title: route.meta.title,
+          icon: route.meta.icon,
+          hidden: route.hidden,
+          frameSrc: route.meta.link,
+          frameBlank: true,
+        },
+      };
+    } else if (route.children?.length) {
+      return {
+        path: route.path,
+        name: route.name,
+        redirect: '/',
+        meta: {
+          title: route.meta.title,
+          icon: route.meta.icon,
+          hidden: route.hidden,
+        },
+        children: standardizeRoutes(route.children, route.name),
+      };
+    } else {
+      const view = views[`/src/views/${route.component}.vue`];
+      return {
+        path: route.path,
+        name: `${parentName}${route.name}`,
+        component: view ? markRaw(view.default) : () => import('@/views/Placeholder.vue'),
+        meta: {
+          title: route.meta.title,
+          icon: route.meta.icon,
+          hidden: route.hidden,
+        },
+      };
     }
   });
 }
@@ -65,8 +51,7 @@ export async function initializeRouter() {
 
   try {
     const { data } = await getRouters();
-    console.log(standardizeRoutes(data));
-    appStore.setRoutes([1, 2, 3]);
+    appStore.setRoutes(standardizeRoutes(data));
   } catch {
     appStore.resetApp();
   }
