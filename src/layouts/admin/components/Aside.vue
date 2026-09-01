0185705a5a629e39b7104022b5abb139c19c9d3a
Min <solunar@vip.qq.com>
2026-09-01 15:39:43 +0800

    Update

--- src/layouts/admin/components/Aside.vue
+++ src/layouts/admin/components/Aside.vue
@@ -12,7 +12,7 @@ console.log(menus.value);
 
 <template>
   <aside class="border-border bg-primary-light-9 border-r">
-    <div :class="[expanded ? 'w-60' : 'w-10']" class="flex h-full flex-col duration-200">
+    <div :class="[expanded ? 'w-50' : 'w-10']" class="flex h-full flex-col duration-200">
       <div class="border-border flex h-10 shrink-0 items-center justify-end border-b px-3">
         <component
           :is="expanded ? PanelLeftClose : PanelLeftOpen"
@@ -28,7 +28,17 @@ console.log(menus.value);
               <div class="flex-1">{{ menu.meta?.title }}</div>
               <ChevronDown v-if="menu.children?.length" :class="[menu.meta?.expanded ? 'rotate-180' : '']" class="shrink-0 duration-200" size="16" />
             </div>
+            <div v-if="menu.children?.length">
+              <div class="bg-background">
+                <div v-for="(item, idx) in menu.children" :key="idx">
+                  <div class="flex h-10 items-center px-3">
+                    <div class="flex-1">{{ item.meta?.title }}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+          <div class="h-10"></div>
         </el-scrollbar>
       </div>
     </div>
