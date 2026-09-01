c4a9a397979e453e924773958cb94e624d093423
Min <solunar@vip.qq.com>
2026-09-01 14:09:32 +0800

    Update

--- src/layouts/admin/components/Aside.vue
+++ src/layouts/admin/components/Aside.vue
@@ -1,8 +1,28 @@
+<script setup>
+import { PanelLeftClose, PanelLeftOpen } from '@lucide/vue';
+
+import { useAppStore } from '@/stores';
+
+const appStore = useAppStore();
+const expanded = ref(true);
+
+console.log(appStore.routes);
+</script>
+
 <template>
-  <aside class="border-border bg-primary-light-9 flex w-60 shrink-0 flex-col border-r">
-    <div class="border-border h-10 border-b"></div>
-    <div class="flex-1 overflow-y-auto">
-      <div class="h-500"></div>
+  <aside class="border-border bg-primary-light-9 border-r">
+    <div :class="[expanded ? 'w-60' : 'w-10']" class="flex h-full shrink-0 flex-col duration-200">
+      <div class="border-border flex h-10 items-center justify-end border-b px-3">
+        <component
+          :is="expanded ? PanelLeftClose : PanelLeftOpen"
+          @click="expanded = !expanded"
+          class="text-regular-foreground hover:text-brand cursor-pointer duration-200"
+          size="16"
+        />
+      </div>
+      <div class="flex-1 overflow-y-auto">
+        <div class="h-500"></div>
+      </div>
     </div>
   </aside>
 </template>
