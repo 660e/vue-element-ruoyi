90fbf0ea9dd8bc3365393b91178f4a93dce9e91d
Min <solunar@vip.qq.com>
2026-09-01 11:30:09 +0800

    Update

--- src/layouts/admin/components/Breadcrumb.vue
+++ src/layouts/admin/components/Breadcrumb.vue
@@ -1,9 +1,8 @@
-<script setup>
-const route = useRoute();
-
-console.log(route.matched);
-</script>
-
 <template>
-  <div>{{ $route.matched.length }}</div>
+  <div class="text-regular-foreground flex items-center gap-1.5 text-sm leading-none">
+    <template v-for="(item, index) in $route.matched.slice(1, $route.matched.length)" :key="item.name">
+      <div v-if="index" class="font-mono">/</div>
+      <div>{{ item.meta?.title }}</div>
+    </template>
+  </div>
 </template>
