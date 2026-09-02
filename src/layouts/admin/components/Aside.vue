1682025631e2e1cc71f8352963a98b5022d60fe4
Min <solunar@vip.qq.com>
2026-09-02 09:40:43 +0800

    Update

--- src/layouts/admin/components/Aside.vue
+++ src/layouts/admin/components/Aside.vue
@@ -52,35 +52,33 @@ function to(menu) {
         />
       </div>
       <div class="flex-1 overflow-auto">
-        <el-scrollbar>
-          <div v-for="(menu, index) in menus" class="border-border border-b" :key="index">
-            <div @click="to(menu)" class="flex h-10 cursor-pointer items-center gap-1.5 px-3 duration-200 hover:bg-blue-100">
-              <Info class="shrink-0" size="16" />
-              <div class="flex-1 leading-none">{{ menu.meta?.title }}</div>
-              <ChevronDown v-if="menu.children?.length" :class="[menu.meta?.expanded ? 'rotate-180' : '']" class="shrink-0 duration-200" size="16" />
-            </div>
-            <div
-              v-if="menu.children?.length"
-              :style="{ gridTemplateRows: menu.children?.length && menu.meta?.expanded ? '1fr' : '0fr' }"
-              class="grid duration-200"
-            >
-              <div class="bg-background overflow-hidden">
-                <div v-for="(item, idx) in menu.children" :key="idx">
-                  <div
-                    :class="[item.name === $route.name ? 'text-brand' : '']"
-                    @click="to(item)"
-                    class="relative flex h-10 cursor-pointer items-center gap-1.5 px-3 duration-200 hover:bg-neutral-100"
-                  >
-                    <div v-if="item.name === $route.name" class="border-brand absolute inset-0 border-t border-r-4 border-b"></div>
-                    <div class="w-4"></div>
-                    <div class="flex-1 leading-none">{{ item.meta?.title }}</div>
-                  </div>
+        <div v-for="(menu, index) in menus" class="border-border border-b" :key="index">
+          <div @click="to(menu)" class="flex h-10 cursor-pointer items-center gap-1.5 px-3 duration-200 hover:bg-blue-100">
+            <Info class="shrink-0" size="16" />
+            <div class="flex-1 leading-none">{{ menu.meta?.title }}</div>
+            <ChevronDown v-if="menu.children?.length" :class="[menu.meta?.expanded ? 'rotate-180' : '']" class="shrink-0 duration-200" size="16" />
+          </div>
+          <div
+            v-if="menu.children?.length"
+            :style="{ gridTemplateRows: menu.children?.length && menu.meta?.expanded ? '1fr' : '0fr' }"
+            class="grid duration-200"
+          >
+            <div class="bg-background overflow-hidden">
+              <div v-for="(item, idx) in menu.children" :key="idx">
+                <div
+                  :class="[item.name === $route.name ? 'text-brand' : '']"
+                  @click="to(item)"
+                  class="relative flex h-10 cursor-pointer items-center gap-1.5 px-3 duration-200 hover:bg-neutral-100"
+                >
+                  <div v-if="item.name === $route.name" class="border-brand absolute inset-0 border-t border-r-4 border-b"></div>
+                  <div class="w-4"></div>
+                  <div class="flex-1 leading-none">{{ item.meta?.title }}</div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="h-10"></div>
-        </el-scrollbar>
+        </div>
+        <div class="h-10"></div>
       </div>
     </div>
   </aside>
