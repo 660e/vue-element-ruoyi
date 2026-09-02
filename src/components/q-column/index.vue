b7c4f24570f77983000c082c4bae8238968ea671
Min <solunar@vip.qq.com>
2026-09-02 15:00:11 +0800

    Update

--- src/components/q-column/index.vue
+++ src/components/q-column/index.vue
@@ -1,3 +1,23 @@
+<script setup>
+defineOptions({ name: 'QColumn' });
+
+const { dict } = defineProps({
+  dict: { type: String, default: null },
+});
+
+const dictList = ref([]);
+
+onMounted(async () => {
+  if (dict) {
+    dictList.value = [];
+  }
+});
+
+function formatter(_, __, cellValue) {
+  return cellValue;
+}
+</script>
+
 <template>
-  <el-table-column />
+  <el-table-column :formatter="formatter" />
 </template>
--- src/views/system/dict/index.vue
+++ src/views/system/dict/index.vue
@@ -6,7 +6,7 @@ import { getDictTypeList } from '@/api/system/dict.js';
   <q-table :request="getDictTypeList">
     <q-column label="字典名称" prop="dictName" width="200" />
     <q-column label="字典类型" prop="dictType" width="200" />
-    <q-column label="状态" prop="status" width="100" />
+    <q-column dict="sys_normal_disable" label="状态" prop="status" width="100" />
     <q-column label="备注" min-width="200" prop="remark" />
     <q-column label="创建时间" prop="createTime" width="200" />
   </q-table>
