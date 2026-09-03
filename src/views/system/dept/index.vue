fa379acd7905ede58f36f2360d9efa1e7f83a80e
Min <solunar@vip.qq.com>
2026-09-03 10:56:32 +0800

    Update

--- src/views/system/dept/index.vue
+++ src/views/system/dept/index.vue
@@ -4,7 +4,7 @@ import { getDeptList } from '@/api/system/dept.js';
 
 <template>
   <q-table row-key="deptId" :expand-row-keys="['100']" :pagination="{ rowsKey: 'data', hidden: true }" :request="getDeptList">
-    <q-column label="部门名称" min-width="200" prop="deptName" />
+    <q-column label="机构名称" min-width="200" prop="deptName" />
     <q-column dict="sys_normal_disable" label="状态" prop="status" width="100" />
     <q-column label="创建时间" prop="createTime" width="200" />
   </q-table>
