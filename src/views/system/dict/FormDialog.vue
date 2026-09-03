a42f747720c0a4fcecf9699f1288e135fc0e4a24
Min <solunar@vip.qq.com>
2026-09-03 15:24:38 +0800

    Update

--- src/views/system/dict/FormDialog.vue
+++ src/views/system/dict/FormDialog.vue
@@ -2,18 +2,27 @@
 import { required } from '@/utils';
 
 const visible = ref(true);
+const confirming = ref(false);
 const formRef = ref(null);
 const formData = ref({});
+
+function confirm() {
+  formRef.value.validate((valid) => {
+    if (valid) {
+      console.log(formData.value);
+    }
+  });
+}
 </script>
 
 <template>
-  <q-dialog v-model="visible" title="title" width="500">
+  <q-dialog v-model="visible" v-model:confirming="confirming" @confirm="confirm" title="title" width="500">
     <el-form ref="formRef" :model="formData">
-      <el-form-item label="Activity name" label-position="top" :rules="[required]">
-        <el-input v-model="formData.name" />
+      <el-form-item label="name1" label-position="top" prop="name1" :rules="[required]">
+        <el-input v-model="formData.name1" />
       </el-form-item>
-      <el-form-item label="Activity name" :rules="[required]">
-        <el-input v-model="formData.name" />
+      <el-form-item label="name2" prop="name2" :rules="[required]">
+        <el-input v-model="formData.name2" />
       </el-form-item>
     </el-form>
   </q-dialog>
