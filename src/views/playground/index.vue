f51e1aa8287b3d602eb17602cb22ceefa9ce0fe3
Min <solunar@vip.qq.com>
2026-09-03 16:27:28 +0800

    Update

--- src/views/playground/index.vue
+++ src/views/playground/index.vue
@@ -5,17 +5,22 @@ const formData = ref({});
 </script>
 
 <template>
-  <div class="w-150 pt-50 pl-50">
+  <div class="w-150 pt-20 pl-50">
     <el-form :model="formData">
-      <el-form-item label="name1" label-position="top" prop="name1" :rules="[required]">
-        <el-input v-model="formData.name1" />
+      <el-form-item label="name" label-position="top" prop="name" :rules="[required]">
+        <el-input v-model="formData.name" />
       </el-form-item>
-      <q-item label="name2" label-position="top" prop="name2" :rules="[required]" />
+      <q-item label="name" label-position="top" prop="name" :rules="[required]" />
 
-      <el-form-item label="name3" prop="name3" :rules="[required]">
-        <el-input v-model="formData.name3" />
+      <el-form-item label="name" label-position="left" label-width="150" prop="name" :rules="[required]">
+        <el-input v-model="formData.name" />
       </el-form-item>
-      <q-item label="name4" prop="name4" :rules="[required]" />
+      <q-item label="name" label-position="left" label-width="150" prop="name" :rules="[required]" />
+
+      <el-form-item label="name" label-position="right" label-width="150" prop="name" :rules="[required]">
+        <el-input v-model="formData.name" />
+      </el-form-item>
+      <q-item label="name" label-position="right" label-width="150" prop="name" :rules="[required]" />
     </el-form>
   </div>
 </template>
