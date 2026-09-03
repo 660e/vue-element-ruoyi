4d212762816d13581d3d7db69a6a010f75b31f9c
Min <solunar@vip.qq.com>
2026-09-03 16:49:56 +0800

    Update

--- src/components/q-item/index.vue
+++ src/components/q-item/index.vue
@@ -1,11 +1,17 @@
 <script setup>
+import { Info } from '@lucide/vue';
+
 defineOptions({ name: 'QItem' });
+defineProps({
+  hint: { type: String, default: null },
+});
 </script>
 
 <template>
   <el-form-item class="q-item">
     <template #label="{ label }">
-      <div>{{ label }}</div>
+      <span>{{ label }}</span>
+      <Info size="14" />
     </template>
 
     <el-input />
--- src/views/playground/FormItem.vue
+++ src/views/playground/FormItem.vue
@@ -7,20 +7,26 @@ const formData = ref({});
 <template>
   <div class="flex h-screen items-center justify-center">
     <el-form class="w-100" label-width="150" :model="formData">
-      <el-form-item label="top" label-position="top" prop="top" :rules="[required]">
+      <el-form-item label="标题" label-position="top" prop="top" :rules="[required]">
         <el-input v-model="formData.top" />
       </el-form-item>
-      <q-item label="top" label-position="top" prop="top" :rules="[required]" />
+      <q-item label="标题" label-position="top" prop="top" :rules="[required]" />
+      <q-item hint="This is a hint" label="标题" label-position="top" prop="top" />
+      <q-item label="标题" label-position="top" prop="top" />
 
-      <el-form-item label="left" label-position="left" prop="left" :rules="[required]">
+      <el-form-item label="标题" label-position="left" prop="left" :rules="[required]">
         <el-input v-model="formData.left" />
       </el-form-item>
-      <q-item label="left" label-position="left" prop="left" :rules="[required]" />
+      <q-item label="标题" label-position="left" prop="left" :rules="[required]" />
+      <q-item hint="This is a hint" label="标题" label-position="left" prop="left" />
+      <q-item label="标题" label-position="left" prop="left" />
 
-      <el-form-item label="right" label-position="right" prop="right" :rules="[required]">
+      <el-form-item label="标题" label-position="right" prop="right" :rules="[required]">
         <el-input v-model="formData.right" />
       </el-form-item>
-      <q-item label="right" label-position="right" prop="right" :rules="[required]" />
+      <q-item label="标题" label-position="right" prop="right" :rules="[required]" />
+      <q-item hint="This is a hint" label="标题" label-position="right" prop="right" />
+      <q-item label="标题" label-position="right" prop="right" />
     </el-form>
   </div>
 </template>
