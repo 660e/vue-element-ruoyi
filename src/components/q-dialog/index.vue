a08b86762058dd1d7f628b56f41ce2fc107d96c6
Min <solunar@vip.qq.com>
2026-09-03 13:59:06 +0800

    Update

--- src/components/q-dialog/index.vue
+++ src/components/q-dialog/index.vue
@@ -1,14 +1,16 @@
 <script setup>
+import { X } from '@lucide/vue';
+
 defineOptions({ name: 'QDialog' });
 </script>
 
 <template>
-  <el-dialog align-center destroy-on-close draggable :close-on-click-modal="false" :close-on-press-escape="false">
+  <el-dialog align-center destroy-on-close :close-icon="X" :close-on-click-modal="false" :close-on-press-escape="false">
     <slot></slot>
 
     <template #footer>
-      <el-button type="primary">确定</el-button>
       <el-button>取消</el-button>
+      <el-button type="primary">确定</el-button>
     </template>
   </el-dialog>
 </template>
