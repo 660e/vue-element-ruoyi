b2d476f5facc4e9f807c7974f9ce63238c05d161
Min <solunar@vip.qq.com>
2026-09-03 08:22:58 +0800

    Update

--- src/components/q-column/index.vue
+++ src/components/q-column/index.vue
@@ -1,30 +1,8 @@
 <script setup lang="jsx">
-import { useDict } from '@/hooks';
-
 defineOptions({ name: 'QColumn' });
 
-const { dict } = defineProps({
-  dict: { type: String, default: null },
-});
-
-const { list: getDictList, label: getDictLabel } = useDict();
-const dictList = ref([]);
-
-onMounted(async () => {
-  if (dict) {
-    dictList.value = await getDictList(dict);
-
-    const label = await getDictLabel(dict, '0');
-    console.log(label);
-  }
-});
-
 function formatter(_, __, cellValue) {
-  if (dict) {
-    return <span>{cellValue}</span>;
-  } else {
-    return cellValue;
-  }
+  return <span>{cellValue}</span>;
 }
 </script>
 
--- src/hooks/data.js
+++ src/hooks/data.js
@@ -1,40 +1,53 @@
 import { getDictDataType } from '@/api/system/dict.js';
-import { setSessionStorage, getSessionStorage } from '@/utils';
+import { setSessionStorage, getSessionStorage, is } from '@/utils';
+
+const pendingRequests = new Map();
+const dictCache = ref(new Map());
 
 export function useDict() {
   async function list(type) {
-    const dictList = getSessionStorage(type);
-    if (dictList) {
+    const sessionData = getSessionStorage(type);
+    if (sessionData) {
       try {
-        return JSON.parse(dictList);
-      } catch {
-        return [];
-      }
-    } else {
-      try {
-        const { data } = await getDictDataType(type);
-        const dict = data.map((e) => ({ label: e.dictLabel, value: e.dictValue, tag: e.listClass }));
-
-        setSessionStorage(type, JSON.stringify(dict));
+        const dict = JSON.parse(sessionData);
+        dictCache.value.set(type, dict);
 
         return dict;
       } catch {
         return [];
       }
+    } else if (pendingRequests.has(type)) {
+      return pendingRequests.get(type);
+    } else {
+      const request = (async () => {
+        try {
+          const { data } = await getDictDataType(type);
+          const dict = data.map((e) => ({ label: e.dictLabel, value: e.dictValue, tag: e.listClass }));
+          dictCache.value.set(type, dict);
+          setSessionStorage(type, JSON.stringify(dict));
+
+          return dict;
+        } catch {
+          return [];
+        }
+      })().finally(() => {
+        pendingRequests.delete(type);
+      });
+
+      pendingRequests.set(type, request);
+      return request;
     }
   }
 
-  async function label(type, value) {
-    // const promise = (async () => {
-    //   try {
-    //     const item = (await list(type)).find((e) => e.value === value);
-    //     return item ? item.label : '-';
-    //   } catch {
-    //     return '-';
-    //   }
-    // })();
-
-    return '-';
+  function label(type, value) {
+    const dictList = dictCache.value.get(type);
+    if (is.array(dictList)) {
+      const item = dictList.find((e) => e.value === value);
+      return item ? item.label : '-';
+    } else {
+      list(type);
+      return '-';
+    }
   }
 
   function value() {}
