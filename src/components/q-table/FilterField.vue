93a8bf544d0f79c304849bfafc2e2052d4355080
Min <solunar@vip.qq.com>
2026-09-07 14:03:55 +0800

    Update

--- src/components/q-table/FilterField.vue
+++ src/components/q-table/FilterField.vue
@@ -1,6 +1,5 @@
 <script setup>
-defineEmits(['search']);
-
+const emit = defineEmits(['search']);
 const { parentSlots } = defineProps({ parentSlots: { type: Object } });
 
 const items = computed(() => {
@@ -16,15 +15,20 @@ const items = computed(() => {
 
 function reset() {
   console.log('reset');
+  emit('search');
 }
 </script>
 
 <template>
-  <div class="shrink-0">
+  <div class="q-table-filter-field shrink-0">
     <el-form class="flex gap-3" @submit.prevent="$emit('search')">
       <div class="grid flex-1 grid-cols-4 gap-3">
         <el-form-item v-for="item in items" class="ring-border focus-within:ring-brand rounded-base m-0! ring-1 duration-200" :key="item.prop">
-          <!-- <el-input /> -->
+          <div class="flex w-full">
+            <div class="max-w-1/2 shrink-0 pl-3 text-ellipsis">{{ item.label }}</div>
+
+            <el-input class="flex-1" />
+          </div>
         </el-form-item>
       </div>
       <div class="shrink-0">
@@ -34,3 +38,10 @@ function reset() {
     </el-form>
   </div>
 </template>
+
+<style scoped>
+.q-table-filter-field :deep(.el-input) .el-input__wrapper,
+.q-table-filter-field :deep(.el-select) .el-input__wrapper {
+  box-shadow: none;
+}
+</style>
