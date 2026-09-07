087232af05ceeca686a742a9a5bb4b433a14a109
Min <solunar@vip.qq.com>
2026-09-07 15:52:34 +0800

    Update

---
+++ src/api/system/role.js
@@ -0,0 +1,17 @@
+import { request } from '@/utils';
+
+export function createRole(data) {
+  return request.post('/system/role', { data });
+}
+
+export function deleteRole(roleId) {
+  return request.delete(`/system/role/${roleId}`);
+}
+
+export function updateRole(data) {
+  return request.put('/system/role', { data });
+}
+
+export function getRole(params) {
+  return request.get('/system/role/list', { params });
+}
