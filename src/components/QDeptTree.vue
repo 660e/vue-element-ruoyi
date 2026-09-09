2a2853cf144f5b15224d9bd2a7d8286ea8bb66a3
Min <solunar@vip.qq.com>
2026-09-09 14:46:35 +0800

    Update

--- src/components/QDeptTree.vue
+++ src/components/QDeptTree.vue
@@ -1,13 +1,17 @@
 <script setup>
+import { RefreshCw } from '@lucide/vue';
+
 import { getDept } from '@/api/system/dept.js';
 import { useRestAttrs } from '@/hooks';
 
 defineOptions({ name: 'QDeptTree', inheritAttrs: false });
+defineProps({ background: { type: Boolean, default: true } });
 
 const attrs = useRestAttrs();
+const loading = ref(false);
 const treeRef = ref(null);
 const treeData = ref([]);
-const loading = ref(false);
+const filterText = ref('');
 
 onMounted(() => {
   getTreeData();
@@ -26,9 +30,22 @@ async function getTreeData() {
 
 <template>
   <div v-loading="loading" class="flex flex-col" :class="$attrs.class" :style="$attrs.style">
-    <div class="shrink-0">filter</div>
-    <el-scrollbar>
-      <div class="h-500 bg-red-50" v-bind="attrs"></div>
+    <div class="flex shrink-0 gap-3 pb-3">
+      <el-input v-model="filterText" placeholder="请输入机构名称" />
+      <el-button :icon="RefreshCw" />
+    </div>
+    <el-scrollbar class="rounded-base" :class="[background ? 'bg-neutral-50' : '']">
+      <el-tree
+        class="bg-transparent! py-1.5"
+        node-key="deptId"
+        :data="treeData"
+        :default-expanded-keys="[100]"
+        :expand-on-click-node="false"
+        :props="{ label: 'deptName' }"
+        ref="treeRef"
+        highlight-current
+        v-bind="attrs"
+      />
     </el-scrollbar>
   </div>
 </template>
