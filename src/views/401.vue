0d4ab6ece8d1f2e4dc0de7c4d064a179367b03d9
Min <solunar@vip.qq.com>
2026-09-10 14:24:15 +0800

    Update

--- src/views/401.vue
+++ src/views/401.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>401</div>
+  <div class="flex h-full items-center justify-center text-9xl text-neutral-200">401</div>
 </template>
--- src/views/404.vue
+++ src/views/404.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>404</div>
+  <div class="flex h-full items-center justify-center text-9xl text-neutral-200">404</div>
 </template>
