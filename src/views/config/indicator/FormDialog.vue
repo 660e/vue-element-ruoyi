909fdda7e5ef9d446dfe97f92a852ce60a83aac5
Min <solunar@vip.qq.com>
2026-09-10 14:28:31 +0800

    Update

---
+++ src/views/config/indicator/FormDialog.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ElMessage } from 'element-plus';
+
+import { createCategory, updateCategory } from '@/api/assessment/category.js';
+import { required } from '@/utils';
+
+const emit = defineEmits(['confirm']);
+const visible = ref(false);
+const confirming = ref(false);
+const formRef = ref(null);
+const formData = ref({});
+const categoryData = ref([]);
+
+function open({ row, tree = [], parentId }) {
+  formData.value = row ? { ...row } : { parentId };
+  categoryData.value = tree;
+  visible.value = true;
+}
+
+function confirm() {
+  formRef.value.validate(async (valid) => {
+    if (valid) {
+      confirming.value = true;
+      const request = formData.value.categoryId ? updateCategory : createCategory;
+      try {
+        const { code, msg } = await request({ ...formData.value });
+        if (code === 200) {
+          ElMessage.success(msg);
+          emit('confirm');
+          visible.value = false;
+        }
+      } finally {
+        confirming.value = false;
+      }
+    }
+  });
+}
+
+defineExpose({ open });
+</script>
+
+<template>
+  <q-dialog
+    v-model="visible"
+    v-model:confirming="confirming"
+    width="400"
+    :title="formData.categoryId ? '修改' : '新增'"
+    @cancel="visible = false"
+    @confirm="confirm"
+  >
+    <el-form label-position="top" :model="formData" ref="formRef">
+      <q-item
+        label="上级分类"
+        prop="parentId"
+        :config="{
+          type: 'cascader',
+          options: categoryData,
+          props: { checkStrictly: true, emitPath: false, label: 'categoryName', value: 'categoryId' },
+        }"
+      />
+      <q-item label="分类名称" prop="categoryName" :rules="[required]" />
+      <q-item v-if="!formData.parentId" label="分类编码" prop="categoryCode" />
+      <q-item label="显示顺序" prop="sortOrder" :config="{ type: 'number' }" :rules="[required]" />
+    </el-form>
+  </q-dialog>
+</template>
---
+++ src/views/config/indicator/index.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { getCategoryTree, deleteCategory } from '@/api/assessment/category.js';
+
+import FormDialog from './FormDialog.vue';
+
+const tableRef = ref(null);
+const formDialogRef = ref(null);
+
+async function request(params) {
+  const { data } = await getCategoryTree(params);
+  return { rows: data };
+}
+
+async function handleEdit(row, id) {
+  tableRef.value.setLoading(true);
+  try {
+    const { data } = await getCategoryTree();
+    formDialogRef.value.open({ row, tree: data, parentId: id });
+  } finally {
+    tableRef.value.setLoading(false);
+  }
+}
+</script>
+
+<template>
+  <q-table row-key="categoryId" :pagination="{ hidden: true }" :request="request" ref="tableRef" default-expand-all>
+    <template #header>
+      <el-button type="primary" @click="handleEdit()" plain>新增</el-button>
+    </template>
+
+    <el-table-column label="分类名称" min-width="200" prop="categoryName" :config="{ filter: 'text' }" />
+    <q-column label="分类编码" min-width="200" prop="categoryCode" :config="{ filter: 'text' }" />
+    <q-column label="显示顺序" prop="sortOrder" width="100" />
+    <q-column label="修改时间" prop="updateTime" width="200" />
+    <q-column width="150" operation>
+      <template #default="{ row }">
+        <el-button type="primary" @click="handleEdit(null, row.categoryId)" link>新增</el-button>
+        <el-button type="primary" @click="handleEdit(row)" link>修改</el-button>
+        <q-confirm :content="`分类名称：${row.categoryName}`" :request="() => deleteCategory(row.categoryId)" @confirm="tableRef.refresh()" />
+      </template>
+    </q-column>
+  </q-table>
+
+  <FormDialog @confirm="tableRef.refresh()" ref="formDialogRef" />
+</template>
